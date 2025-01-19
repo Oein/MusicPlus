9f5485aaf5aed3ed6fbf05fea5b0397f24ff29a2
Oein <oein0219@gmail.com>
2025-01-20 00:47:29 +0900

    music kit auth page

---
+++ musicplus/src/ui/routes/Auth/Auth.swift
@@ -0,0 +1,43 @@
+//
+//  Auth.swift
+//  musicplus
+//
+//  Created by oein on 1/19/25.
+//
+
+import SwiftUI
+
+struct AuthPage: View {
+    var body: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Spacer()
+                Text("Welcome back!")
+                    .font(.satoshiBold28)
+                Button(
+                    action: {
+                        Task {
+                            let res = await MusicKitManager.shared.requestAuth()
+                            if res {
+                                PathManager.shared.set_path(path: "::main", qparm: nil)
+                            } else {
+                                print("TODO:: ALERT - AUTH FAILED")
+                            }
+                        }
+                    },
+                    label: {
+                        Text("Authorize with Apple Music")
+                            .padding(.horizontal, 18)
+                            .padding(.vertical, 8)
+                    }
+                )
+                .buttonStyle(.plain)
+                .background(Color("Background-Secondary"))
+                .clipShape(.buttonBorder)
+                Spacer()
+            }
+            Spacer()
+        }
+    }
+}
