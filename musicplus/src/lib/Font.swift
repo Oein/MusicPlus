2dcbf557f3bcf1c9f1b9690c0568e7840611e84c
Oein <oein0219@gmail.com>
2025-01-20 00:46:36 +0900

    add font

---
+++ musicplus/src/lib/Font.swift
@@ -0,0 +1,26 @@
+//
+//  Font.swift
+//  musicplus
+//
+//  Created by oein on 1/19/25.
+//
+
+import SwiftUICore
+
+extension Font {
+    
+    // Bold
+    static let satoshiBold28: Font = .custom("Satoshi-Black", size: 28)
+    static let satoshiBold24: Font = .custom("Satoshi-Black", size: 24)
+    static let satoshiBold18: Font = .custom("Satoshi-Bold", size: 18)
+    static let satoshiBold14: Font = .custom("Satoshi-Bold", size: 14)
+    
+    // Medium
+    static let satoshiMedium18: Font = .custom("Satoshi-Medium", size: 18)
+    static let satoshiMedium16: Font = .custom("Satoshi-Medium", size: 16)
+    
+    // Regular
+    static let satoshiRegular16: Font = .custom("Satoshi-Regular", size: 16)
+    static let satoshiRegular14: Font = .custom("Satoshi-Regular", size: 14)
+    
+}
