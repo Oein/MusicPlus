a23785150cd23d211b2d54c2c378a49c20858ea4
Oein <oein0219@gmail.com>
2025-01-20 00:46:43 +0900

    music kit manager

---
+++ musicplus/src/lib/MusicKitManager.swift
@@ -0,0 +1,49 @@
+//
+//  MusicKitManager.swift
+//  musicplus
+//
+//  Created by oein on 1/18/25.
+//
+import MusicKit
+
+enum NetworkData<T> {
+    case success(data: T)
+    case failure
+}
+
+class MusicKitManager {
+    
+    static let shared = MusicKitManager()
+    private init() {
+    }
+    
+    func requestAuth() async -> Bool {
+        let status = await MusicAuthorization.request()
+        switch status {
+        case .denied:
+            print("Denied")
+            return false;
+        case .authorized:
+            print("Authorized")
+            return true
+        default:
+            print(status)
+            return false;
+        }
+    }
+    
+    func hasPermission() -> Bool {
+        return MusicAuthorization.currentStatus == .authorized;
+    }
+    
+    func getRecommend() async -> NetworkData<MusicItemCollection<MusicPersonalRecommendation>> {
+        let request = MusicPersonalRecommendationsRequest()
+        do {
+            let response = try await request.response()
+            return .success(data: response.recommendations)
+        } catch {
+            print("Error in requesting for recommendations: \(error)")
+            return .failure
+        }
+    }
+}
